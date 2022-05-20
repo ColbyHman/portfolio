63972b4a5b53077fc8df1f2d4128d6c24386df9a
Colby Hillman <colby.hillman@gmail.com>
2022-05-20 18:32:32 -0400

    Added components for homepage cards

---
+++ portfolio_website/src/app/home/home-servers/home-servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-servers',
+  templateUrl: './home-servers.component.html',
+  styleUrls: ['./home-servers.component.scss']
+})
+export class HomeServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
